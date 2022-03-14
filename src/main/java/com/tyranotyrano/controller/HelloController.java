a8d9917289a1a687ed9d663512a94a53c9106050
tyanotyrano <tyrano09@gmail.com>
2022-03-14 11:48:30 +0900

    feat(HelloController.java): /hello-api 요청처리(객체반환) 기능 추가
    /hello-api 요청처리(객체반환) 기능을 추가한다.
    - HelloController : /hello-api 요청처리(객체반환) 기능 추가
    - Hello : /hello-api 요청처리 결과 반환되는 객체

--- src/main/java/com/tyranotyrano/controller/HelloController.java
+++ src/main/java/com/tyranotyrano/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.tyranotyrano.controller;
 
+import com.tyranotyrano.domain.Hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -19,5 +21,19 @@ public class HelloController {
                            Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
     }
 }
---
+++ src/main/java/com/tyranotyrano/domain/Hello.java
@@ -0,0 +1,8 @@
+package com.tyranotyrano.domain;
+
+import lombok.Data;
+
+@Data
+public class Hello {
+    private String name;
+}
