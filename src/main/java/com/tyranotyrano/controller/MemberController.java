6b038784248473003676e37a857466bb40512fc3
tyanotyrano <tyrano09@gmail.com>
2022-03-16 10:22:16 +0900

    feat(MemberController.java): 회원목록 조회를 위한 API 작성
    회원목록 조회를 위한 API 를 작성한다.
    - MemberController : 회원목록 조회를 위한 API 요청 처리
    - memberList.html : 회원목록을 출력하는 뷰

--- src/main/java/com/tyranotyrano/controller/MemberController.java
+++ src/main/java/com/tyranotyrano/controller/MemberController.java
@@ -1,7 +1,10 @@
 package com.tyranotyrano.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,12 +21,12 @@ public class MemberController {
 		this.memberService = memberService;
 	}
 
-	@GetMapping("members/new")
+	@GetMapping("/members/new")
 	public String createForm() {
 		return "members/createMemberForm";
 	}
 
-	@PostMapping("members/new")
+	@PostMapping("/members/new")
 	public String create(CreateMemberRq rq) {
 		Member member = new Member();
 		member.setName(rq.getName());
@@ -31,5 +34,13 @@ public class MemberController {
 		memberService.join(member);
 
 		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String list(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+
+		return "members/memberList";
 	}
 }
