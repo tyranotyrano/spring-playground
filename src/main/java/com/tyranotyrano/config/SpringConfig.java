ec5181b3eb4d507ec43c274e2b27f45969cc7eb5
tyanotyrano <tyrano09@gmail.com>
2022-03-16 15:18:01 +0900

    feat(JdbcMemberRepository.java): Memory DB Repository 를 H2 DB Repository 로 변경
    Memory DB Repository 를 H2 DB Repository 로 변경한다.
    - JdbcMemberRepository : H2 DB 를 사용한 MemberRepository 구현체
    - SpringConfig : Memory DB Repository 를 H2 DB Repository 로 변경

--- src/main/java/com/tyranotyrano/config/SpringConfig.java
+++ src/main/java/com/tyranotyrano/config/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.tyranotyrano.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.tyranotyrano.repository.JdbcMemberRepository;
 import com.tyranotyrano.repository.MemberRepository;
-import com.tyranotyrano.repository.MemoryMemberRepository;
 import com.tyranotyrano.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+	private final DataSource dataSource; // 스프링이 application.yml 에 설정한 내용을 보고 자체적으로 DataSource 빈을 생성한다.
+
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +27,9 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		// 1. Memory DB 로 사용하던 Repository
+		// return new MemoryMemberRepository();
+
+		return new JdbcMemberRepository(dataSource);
 	}
 }
