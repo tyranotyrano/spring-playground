5edad2929c2a00c5f4b88155b27444b725b27948
tyanotyrano <tyrano09@gmail.com>
2022-03-17 10:29:41 +0900

    feat(JdbcTemplateMemberRepository.java): JdbcTemplate 을 사용한 Repository 로 변경
    JdbcTemplate 을 사용한 Repository 로 변경한다.
    - JdbcTemplateMemberRepository : JdbcTemplate 을 사용한 MemberRepository 구현체
    - SpringConfig : 순수 JDBC Repository 를 JdbcTemplate Repository 로 변경

--- src/main/java/com/tyranotyrano/config/SpringConfig.java
+++ src/main/java/com/tyranotyrano/config/SpringConfig.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.tyranotyrano.repository.JdbcMemberRepository;
+import com.tyranotyrano.repository.JdbcTemplateMemberRepository;
 import com.tyranotyrano.repository.MemberRepository;
 import com.tyranotyrano.service.MemberService;
 
@@ -30,6 +30,10 @@ public class SpringConfig {
 		// 1. Memory DB 로 사용하던 Repository
 		// return new MemoryMemberRepository();
 
-		return new JdbcMemberRepository(dataSource);
+		// 2. H2 DB + 순수 Jdbc Repository
+		// return new JdbcMemberRepository(dataSource);
+
+		// 3. H2 DB + JdbcTemplate Repository
+		return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
